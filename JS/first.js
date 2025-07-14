9d64e6950051f4e50682bb57fec1fe3292ff9a51
Debesh Behera <anshubehera112@gmail.com>
2025-07-14 11:17:46 +0530

    var let const

---
+++ JS/first.js
@@ -0,0 +1,41 @@
+//  console.log("Namaste Duniya");
+
+
+//  var
+var age = 90;
+var age = 50;
+function calc(){
+    var age = 60;
+    
+    console.log(age);
+}
+console.log(age);
+calc();
+
+
+
+
+
+// //   let
+
+// let age = 90;
+// age = 'Debesh';   //Dynamic Type Language
+// function calc(){
+//     let age = 60;   
+//     console.log(age);
+// }
+// console.log(age);
+// calc();
+
+
+
+
+//   const
+
+const ageOfChild = 10;
+// age = 40; //not possible
+// {
+//     const age = 80;
+//     console.log(age);
+// }
+console.log(ageOfChild);
